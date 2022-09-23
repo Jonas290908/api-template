f2cc476fb9675396bec880881abd1784ba820d3a
Jonas <67281619+Jonas290908@users.noreply.github.com>
2022-09-23 16:20:17 +0200

    Initial Commit

---
+++ src/httpLogger.ts
@@ -0,0 +1,19 @@
+import morgan, { StreamOptions } from "morgan";
+
+import Logger from "./logger";
+
+const stream: StreamOptions = {
+  write: (message) => Logger.http(message.substring(0,message.lastIndexOf('\n'))),
+};
+
+const skip = () => {
+  const env = process.env.NODE_ENV || "dev";
+  return env !== "dev";
+};
+
+const HttpLogMiddleware = morgan(
+  ":method :url :status :res[content-length] - :response-time ms",
+  { stream, skip }
+);
+
+export default HttpLogMiddleware;
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import express, { Express } from 'express';
+import morgan, { StreamOptions } from "morgan";
+
+import Logger from './logger';
+import HttpLogMiddleware from './httpLogger'
+
+const app: Express = express();
+app.use(HttpLogMiddleware);
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+  
+app.listen(8000, () => {
+    Logger.info("Starting server on port 8000");
+})
+  
